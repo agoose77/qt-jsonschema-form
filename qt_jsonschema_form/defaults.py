5d85aeed635de88f7bfcf0b3c4f7c11ae570cc0b
Arthur Milchior <Arthur@Milchior.fr>
2020-03-05 03:27:51 +0100

    use a dict to send type to their default method

--- qt_jsonschema_form/defaults.py
+++ qt_jsonschema_form/defaults.py
@@ -12,10 +12,16 @@ def object_defaults(schema):
 def array_defaults(schema):
     items_schema = schema['items']
     if isinstance(items_schema, dict):
+        # in this case, it should be a list of items_schema
         return []
 
+    # in this case, it's a tuple.
     return [compute_defaults(s) for s in schema["items"]]
 
+defaults = {
+    "array": array_defaults,
+    "object": object_defaults,
+}
 
 def compute_defaults(schema):
     if "default" in schema:
@@ -27,10 +33,7 @@ def compute_defaults(schema):
 
     schema_type = schema["type"]
 
-    if schema_type == "object":
-        return object_defaults(schema)
-
-    elif schema_type == "array":
-        return array_defaults(schema)
+    if schema_type in defaults:
+        return defaults[schema_type](schema)
 
     return None
